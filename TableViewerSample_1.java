56e3572fdd726b8ebe849722c88acb6f4d021baf
Dawid S <60789011+davids-91@users.noreply.github.com>
2020-04-09 15:59:01 +0200

    Create TableViewerSample_1.java

---
+++ TableViewerSample_1.java
@@ -0,0 +1,28 @@
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class TableViewerSample_1 {
+    public TableViewerSample_1(Shell shell)
+    {
+        AppPersonViewer personViewer = new AppPersonViewer(shell, SWT.BORDER|SWT.V_SCROLL|SWT.FULL_SELECTION);
+        DataModel model = new DataModel(20);
+        personViewer.setInput(model);
+    }
+
+    public static void main(String[] args) {
+        Display display = new Display ();
+        Shell shell = new Shell(display);
+        shell.setLayout(new FillLayout());
+        new TableViewerSample_1(shell);
+        shell.open ();
+
+        while (!shell.isDisposed ()) {
+            if (!display.readAndDispatch ()) display.sleep ();
+        }
+
+        display.dispose ();
+    }
+
+}
