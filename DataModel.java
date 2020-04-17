55699b13afb9ed3ad8a4d290aacb9e4facd16655
Dawid S <60789011+davids-91@users.noreply.github.com>
2020-04-17 16:00:40 +0200

    Update DataModel.java

--- DataModel.java
+++ DataModel.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.swt.widgets.Shell;
+
 public class DataModel {
     private int samples;
+    protected static Shell shell;
     public DataModel(int samples) {
         this.samples = samples;
     }
 
-    List<Data> getData()
-    {
+    List<Data> getData() {
         List<Data> persons = new ArrayList<Data>();
-        for(int i=0; i<samples; i++)
-            persons.add(Data.createRandomData());
+        for(int i = 0; i < samples; i++)
+//            persons.add(Data.createRandomData());
+        	persons.add(new Data(Data.FIRST[i], Data.SECOND[i], Data.THIRD[i]));
         return persons;
     }
 }
